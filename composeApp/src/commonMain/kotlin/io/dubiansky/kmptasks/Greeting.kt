aa7d89903dd215d66b8541092c09439862855aa1
nicodubi <nicodubi@hotmail.com>
2025-03-11 02:12:50 -0300

    Remove obsolete platform-specific and Greeting files

--- composeApp/src/commonMain/kotlin/io/dubiansky/kmptasks/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package io.dubiansky.kmptasks
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/io/dubiansky/kmptasks/Platform.kt
+++
@@ -1,7 +0,0 @@
-package io.dubiansky.kmptasks
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
