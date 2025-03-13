0d8e21bead3c035b62eb9f3e389a37a3a15e35b1
nicodubi <nicodubi@hotmail.com>
2025-03-13 19:00:48 -0300

    Improve TaskList efficiency by adding stable keys in LazyColumn

--- composeApp/src/commonMain/kotlin/io/dubiansky/kmptasks/feature/tasklist/presentation/TaskListScreen.kt
+++ composeApp/src/commonMain/kotlin/io/dubiansky/kmptasks/feature/tasklist/presentation/TaskListScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -63,8 +64,8 @@ fun TaskList(modifier: Modifier = Modifier, taskList: List<Task>) {
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(Dimens.paddingTaskItem)
     ) {
-        items(taskList.size) { task ->
-            TaskItem(task = taskList[task])
+        items(items = taskList, key = { it.id }) { task ->
+            TaskItem(task = task)
         }
     }
 }
