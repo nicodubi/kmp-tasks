82421c99523bb600d9545ea24e2e97b5264cb0bd
nicodubi <nicodubi@hotmail.com>
2025-03-17 01:49:57 -0300

    Update TaskRepository and DAO to support task details

--- composeApp/src/commonMain/kotlin/io/dubiansky/kmptasks/core/common/data/database/TaskDao.kt
+++ composeApp/src/commonMain/kotlin/io/dubiansky/kmptasks/core/common/data/database/TaskDao.kt
@@ -10,11 +10,14 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TaskDao {
     @Query("SELECT * FROM TASKS ORDER BY id ASC")
-    fun getAllTasks(): Flow<List<TaskEntity>>
+    fun getAllTasks(): Flow<List<TaskEntity>> //is not suspend fun because Flow handle the background I/O
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTask(task: TaskEntity)
 
     @Delete
     suspend fun deleteTask(task: TaskEntity)
+
+    @Query("SELECT * FROM TASKS WHERE id = :id") //is not suspend fun because Flow handle the background I/O
+    fun getTaskDetails(id: Long): Flow<TaskEntity>
 }
